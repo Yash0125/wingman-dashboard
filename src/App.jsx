57c8a4274bea86d9e292390dcbe3fbd68162ede7
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-12-20 01:01:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+const Navigation = () => {
+  return (
+    <div className="bg-[#115E56] h-[100px] w-14">
+      <h1>Navigation</h1>
+    </div>
+  );
+};
+
+export default Navigation;
