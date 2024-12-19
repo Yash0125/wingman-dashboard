637d5e765e2a43cbc5f3b786a9abd13a80bac703
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-12-20 01:44:01 +0530

    feat: navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-screen">
       <Navigation />
     </div>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,68 @@
+import Logo from "./../assets/Logo.png";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+
 const Navigation = () => {
+  const [selectedIcon, setSelectedIcon] = useState("home");
+
+  const navIcons = [
+    {
+      id: "home",
+      icon: "material-symbols:home-rounded",
+      label: "Home",
+    },
+    {
+      id: "messages",
+      icon: "mage:message-round-fill",
+      label: "Messages",
+    },
+    {
+      id: "groups",
+      icon: "ic:outline-groups",
+      label: "Groups",
+    },
+  ];
+
   return (
-    <div className="bg-[#115E56] h-[100px] w-14">
-      <h1>Navigation</h1>
+    <div className="flex flex-col bg-[#115E56] h-screen w-14 justify-between items-center">
+      <div className="flex flex-col items-center">
+        <div className="p-2">
+          <img src={Logo} alt="" className="h-10 w-10 bg-cover" />
+        </div>
+
+        <div className="h-[1px] w-10 bg-[#134E48] border-t border-[#134E48] my-5 mx-1" />
+        <div className="mt-2 flex flex-col gap-[22px]">
+          {navIcons.map((item) => (
+            <div
+              key={item.id}
+              onClick={() => setSelectedIcon(item.id)}
+              className={`h-[36px] w-[36px] rounded-[8px] flex justify-center items-center cursor-pointer transition-all duration-200 ${
+                selectedIcon === item.id ? "bg-white" : ""
+              }`}
+            >
+              <Icon
+                icon={item.icon}
+                width="20"
+                height="20"
+                className={
+                  selectedIcon === item.id ? "text-[#115E56]" : "text-[#CCFBEF]"
+                }
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div
+        className={`h-[36px] w-[36px] rounded-[8px] flex justify-center items-center cursor-pointer p-2 pb-10`}
+      >
+        <Icon
+          icon="icon-park-solid:setting"
+          width="20"
+          height="20"
+          className="text-[#CCFBEF]"
+        />
+      </div>
     </div>
   );
 };
